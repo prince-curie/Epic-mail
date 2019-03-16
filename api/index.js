3dd2b501c4ee8b1912360f7b70511f0fe237604b
prince-curie <prince.chibuike@gmail.com>
2019-03-16 06:28:08 +0100

    chore(heroku) edit files for heroku - edit readme - edit index.js - edit package.json
    fixes #164653439

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import userRoutes from './routes/user.route';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 
@@ -10,7 +12,7 @@ app.get('/', (req, res) => res.send({ data: 'Welcome to EPIC-MAIL' }));
 app.use('/api/v1/', userRoutes);
 
 /* istanbul ignore next */
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log('App is running.....'));
 
